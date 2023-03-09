ae4cf9cf7bd46a1574b28e85a3ed306c6fb554bd
Antoine2308 <antoinesoumet@gmail.com>
2023-03-09 16:51:54 +0100

    Essai4

--- tilesets/Campus.tsx
+++ tilesets/Campus.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Campus" tilewidth="32" tileheight="32" tilecount="702" columns="27">
+<tileset version="1.9" tiledversion="1.9.2" name="campus" tilewidth="32" tileheight="32" tilecount="702" columns="27">
  <image source="campus.png" width="890" height="840"/>
 </tileset>
