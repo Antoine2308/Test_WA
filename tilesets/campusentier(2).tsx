8264a209a93893f960eebc2c96cf1b116e78d34b
Antoine2308 <antoinesoumet@gmail.com>
2023-03-09 15:44:02 +0100

    Essai2

---
+++ tilesets/campusentier(2).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="campusentier(2)" tilewidth="32" tileheight="32" tilecount="702" columns="27">
+ <image source="campusentier(2).JPG" width="890" height="840"/>
+</tileset>
