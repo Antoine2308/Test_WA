0228b5b16587cbf9d62ab20f2d212eb6c2d6e23f
Antoine2308 <antoinesoumet@gmail.com>
2023-03-09 15:36:15 +0100

    Creation de la carte test

---
+++ tilesets/Fond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Fond" tilewidth="32" tileheight="32" tilecount="702" columns="27">
+ <image source="../../../Master SIGAT Rennes 2/Atelier/campusentier(2).JPG" width="890" height="840"/>
+</tileset>
